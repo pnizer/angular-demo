6ace565848271c2340fba842786e60f03901f5d2
Philippe Nizer <pnizer@gmail.com>
2014-11-14 07:57:24 -0200

    Criado botão limpar

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,24 @@ angular.module('demo', [])
    .controller('MyCtrl', function($scope) {
         $scope.lista = localStorage.data ?
             JSON.parse(localStorage.data) : [];
+        console.log($scope.lista);
+
+        function nextId() {
+            var id = localStorage.seq ? +localStorage.seq : 1;
+            localStorage.seq = '' + (id + 1)
+            return id
+        }
 
         $scope.add = function() {
             $scope.lista.push({
+                id: nextId(),
                 nome: $scope.nome
             });
             $scope.nome = '';
+        };
+
+        $scope.limpar = function() {
+            $scope.lista = $scope.lista.filter(function(todo) { return !todo.completada; })
         };
 
         $scope.$watch('lista', function() {
